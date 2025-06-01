c9340be0fae606fa672e7e7a1cf8e92769851737
ravee2001 <s6406021620076@email.kmutnb.ac.th>
2025-06-01 21:21:37 +0700

    save 3 done

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ const app = express();
 // กำหนดพอร์ตจาก .env หรือใช้ 3000 ถ้าไม่มี
 const port = process.env.PORT || 3000;
 
+app.use(express.json()); // เปิดรับ JSON body
+
+// นำ route จากไฟล์ CRUDBokkNoDB.js มาใช้
+require('./CRUDBokkNoDB')(app);
+
+
+
 app.get('/', (req, res) =>{
     res.send('Hello World from Express!!!');
 });
 
-app.listen(port,() => {
-    console.log(`Server listening at http://localhost:${port}`);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
